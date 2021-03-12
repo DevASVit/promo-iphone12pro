c7014bd776848224ab0aca84443a208d75515cc1
DevASVit <devasvit@gmail.com>
2021-03-12 16:32:14 +0200

    add accordion

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cardDetailChangeElems.forEach((btn, i) => {
       btn.addEventListener('click', () => {
-        if(!btn.classList.contains('active')) {
+        if (!btn.classList.contains('active')) {
           deactive();
           btn.classList.add('active');
           cardDetailsTitleElem.textContent = data[i].name;
@@ -48,5 +48,65 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+  const accordion = () => {
+    const characteristicsListElem = document.querySelector('.characteristics__list');
+    const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+    
+    characteristicsItemElems.forEach((elem) => {
+      if (elem.children[1].classList.contains('active')) {
+        elem.children[1].style.height = `${elem.children[1].scrollHeight}px`;
+      }
+    });
+
+    const open = (button, dropDown) => {
+      closeAllDrops(button, dropDown);
+      dropDown.style.height = `${dropDown.scrollHeight}px`;
+      button.classList.add('active');
+      dropDown.classList.add('active');
+    };
+
+    const close = (button, dropDown) => {
+      button.classList.remove('active');
+      dropDown.classList.remove('active');
+      dropDown.style.height = '';
+    };
+
+    const closeAllDrops = (button, dropDown) => {
+      characteristicsItemElems.forEach((elem) => {
+        if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+          close(elem.children[0], elem.children[1]);
+        }
+      });
+    };
+
+    characteristicsListElem.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target.classList.contains('characteristics__title')) {
+        const parent = target.closest('.characteristics__item');
+        const description = parent.querySelector('.characteristics__description');
+        description.classList.contains('active') ? 
+          close(target, description) : 
+            open(target, description);
+      }
+    });
+
+    document.body.addEventListener('click', (event) => {
+      const target = event.target;
+      if(!target.closest('.characteristics__list')) {
+        closeAllDrops();
+      }
+    });
+  };
+
+  const modal = () => {
+    const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
+    const modal = document.querySelector('.modal');
+
+    cardDetailsButtonBuy.addEventListener('click', () => {
+      modal.classList.add('open');
+    });
+  };
+
   tabs();
+  accordion();
 });
